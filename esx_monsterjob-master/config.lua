fca951de362d7d588d24e13682c5fe86a3c15e5b
gaguio21 <48203635+gaguio21@users.noreply.github.com>
2019-08-30 14:43:04 -0400

    Add files via upload

---
+++ esx_monsterjob-master/config.lua
@@ -0,0 +1,68 @@
+﻿Config                            = {}
+Config.DrawDistance               = 100.0
+Config.MaxInService               = -1
+Config.EnablePlayerManagement     = true
+Config.EnableSocietyOwnedVehicles = false
+Config.Locale                     = 'fr'
+
+Config.Zones = {
+
+	Cafeinefarm = {
+		Pos   = {x = 1713.623, y = 4724.622, z = 0},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Récolte de raisin",
+		Type  = 1
+	},
+
+
+	Traitementmonsterverte = {
+		Pos   = {x = 819.625, y = 4724.622, z = 0},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Traitement du Vin",
+		Type  = 1
+	},
+	
+	Sellmonster = {
+		Pos   = {x = -341.902, y = 6199.499, z = 0},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Vente des produits",
+		Type  = 1
+	},
+
+	MonsterActions = {
+		Pos   = {x = -1912.372, y = 2072.8010, z = 139.387},
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Point d'action",
+		Type  = 0
+	 },
+	  
+	VehicleSpawner = {
+		Pos   = {x = -1889.653, y = 2050.071, z = 139.985},
+		Size = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Garage véhicule",
+		Type  = 0
+	},
+
+	VehicleSpawnPoint = {
+		Pos   = {x = -1903.984, y = 2058.367, z = 139.722},
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Spawn point",
+		Type  = -1
+	},
+
+	VehicleDeleter = {
+		Pos   = {x = -1913.550, y = 2030.590, z = 139.738},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Ranger son véhicule",
+		Type  = 0
+	}
+
+}
+
